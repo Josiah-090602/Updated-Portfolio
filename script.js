c48019b99b948ece5e514d28ebb09436a13f25fb
unknown <gunaorusty@gmail.com>
2023-12-14 09:50:30 +0800

    Fix: modify nav-links active status

--- script.js
+++ script.js
@@ -1,77 +1,80 @@
 const navLink = document.querySelectorAll('.nav-link')
+const pageSection = document.querySelectorAll('.page-section')
 const bars = document.querySelector('.bars')
 const navbar = document.querySelector('.navbar-nav')
 const contactLink = document.querySelector('.nav-item:last-child')
- 
 
+// nav-links
 
 navLink.forEach(nav => {
-    nav.addEventListener("click", () => {
-        removeActive()
-        nav.classList.add('active')
-    })
-}) 
+  nav.addEventListener("click", e => {
+    removeActiveClasses()
+    nav.classList.add('active')
+  })
+})
 
-const removeActive = () => {
-    navLink.forEach((nav, index) => {
-        nav.classList.remove('active')
-    })
+const removeActiveClasses = () => {
+  navLink.forEach(nav => {
+    nav.classList.remove('active')
+  })
 }
 
+// navbar mobile responsive
 
-bars.addEventListener("click", ()=>{
-    if (navbar.classList.contains('expanded')) {
-        navbar.classList.remove('expanded')
-    } else {
-        contactLink.classList.remove('position-absolute')
-        navbar.classList.add('expanded')   
-    }  
+bars.addEventListener("click", () => {
+  if (navbar.classList.contains('expanded')) {
+    navbar.classList.remove('expanded')
+  } else {
+    contactLink.classList.remove('position-absolute')
+    navbar.classList.add('expanded')
+  }
 })
 
 //TypWriter Function
+
 function typeWriter(sentence, containerId, callback) {
-    document.getElementById(containerId);
-    const textElement = document.getElementById('typer');
-    const cursorElement = document.getElementById('cursor');
-  
-    let charIndex = 0;
-    textElement.textContent = '';
-    cursorElement.style.display = 'block';
-  
-    const typingInterval = setInterval(() => {
-      if (charIndex < sentence.length) {
-        textElement.textContent += sentence[charIndex];
-        charIndex++;
-      } else {
-        clearInterval(typingInterval);
-        cursorElement.style.display = 'none';
-        setTimeout(() => {
-          textElement.textContent = '';
-          callback();
-        }, 2000);
-      }
-    }, 70);
-  }
-  
-  const sentences = [
-    'Fullstack Developer',
-    'UI/UX designer',
-    'Freelancer',
-  ];
-  
-  let index = 0;
-  
-  function typeSentences() {
-    if (index < sentences.length) {
-      typeWriter(sentences[index], 'text-container', typeSentences);
-      index++;
+  document.getElementById(containerId);
+  const textElement = document.getElementById('typer');
+  const cursorElement = document.getElementById('cursor');
+
+  let charIndex = 0;
+  textElement.textContent = '';
+  cursorElement.style.display = 'block';
+
+  const typingInterval = setInterval(() => {
+    if (charIndex < sentence.length) {
+      textElement.textContent += sentence[charIndex];
+      charIndex++;
     } else {
-      index = 0;
-      typeSentences();
+      clearInterval(typingInterval);
+      cursorElement.style.display = 'none';
+      setTimeout(() => {
+        textElement.textContent = '';
+        callback();
+      }, 2000);
     }
+  }, 70);
+}
+
+const sentences = [
+  'Fullstack Developer',
+  'UI/UX designer',
+  'Freelancer',
+];
+
+let index = 0;
+
+function typeSentences() {
+  if (index < sentences.length) {
+    typeWriter(sentences[index], 'text-container', typeSentences);
+    index++;
+  } else {
+    index = 0;
+    typeSentences();
   }
-  
-  typeSentences();
+}
+
+typeSentences();
 
 
 
