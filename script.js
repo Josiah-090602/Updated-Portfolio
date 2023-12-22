9a02a29b319ca8a3dbb62ae53699ee7f4e7bdc29
unknown <gunaorusty@gmail.com>
2023-12-22 23:44:27 +0800

    Feat: add scroll animation

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const navLink = document.querySelectorAll('.nav-link')
 const pageSection = document.querySelectorAll('.page-section')
 const bars = document.querySelector('.bars')
+const nav = document.querySelector('.navbar')
 const navbar = document.querySelector('.navbar-nav')
 const contactLink = document.querySelector('.nav-item:last-child')
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > nav.offsetHeight) {
+    nav.classList.add('scrolled')
+  } else {
+    nav.classList.remove('scrolled')
+  }
+})
 
 // nav-links
 
@@ -79,6 +88,46 @@ function typeSentences() {
 }
 
 typeSentences();
+
+// scrolling animation
+
+function smoothScroll(target, duration) {
+  const targetElement = document.querySelector(target);
+  const navbarHeight = document.querySelector('.navbar').offsetHeight;
+  const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+  const startPosition = window.scrollY;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const ease = easeInOutCubic(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, ease);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  // Easing function
+  function easeInOutCubic(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t * t + b;
+    t -= 2;
+    return (c / 2) * (t * t * t + 2) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const target = link.getAttribute('href');
+    const duration = 400;
+    smoothScroll(target, duration);
+  });
+});
 
 
 
